711593a74ac938aa43c06d843a8efbbfc25faa85
Guilherme Aristeu <guilhermearisteu@gmail.com>
2021-09-27 19:35:52 -0300

    correção de erro do control

--- View/Control/quartos.php
+++ View/Control/quartos.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-require_once("../Model/quarto.php");
-require_once("../Model/quartoDAO.php");
+require_once("../../Model/quarto.php");
+require_once("../../Model/quartoDAO.php");
 
 $acao = $_GET["acao"];
 $quartoDAO = new QuartoDAO();
 
 if ($acao == 'listar'){
     $_SESSION["quartos"] = $quartoDAO->recuperarTodos();
-    header('Location: ../View/acomodações.php');
+    header('Location: ../acomodações.php');
 }
